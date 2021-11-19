a1ebec229f5d6e877bbcb544406a9067cfe36a26
Huseyin Cem Akyuz <hsyncmakyz@gmail.com>
2021-11-19 21:31:38 +0300

    .

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,16 @@
 import React from 'react'
 
-import { ExampleComponent } from 'bordeux'
+import { ExampleComponent, Button } from 'bordeux'
 import 'bordeux/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return (
+    <>
+      <ExampleComponent text="Merhaba Dünya😄" />
+      <Button text='Click' onclick={() => alert('adsada')}/>
+
+    </>
+  )
 }
 
 export default App
